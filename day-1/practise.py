158ec9cbe9d0c955cdb29b1357afa5badec09fcb
vaidehiadhi <vaidehiadhi2003@gmail.com>
2025-08-13 19:29:04 +0530

    practise questions

---
+++ day-1/practise.py
@@ -0,0 +1,7 @@
+print("python is\n \t fun!")
+print("A", "B", "C", sep="-")
+first_name = "vaidehi"
+last_name = "adhi"
+print(first_name, last_name, sep="-")
+list_col = ["yellow", "blue", "red"]
+print(list_col[0])
