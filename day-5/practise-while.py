57d3f904edb03cd90f141d0f76b383047371dbf0
vaidehiadhi <vaidehiadhi2003@gmail.com>
2025-08-20 20:13:05 +0530

    removed comments

--- day-5/practise-while.py
+++ day-5/practise-while.py
@@ -1,50 +1,50 @@
 # Counting Up
 # Write a while loop that prints numbers from 1 to 10.
 
-# i = 0
-# while i <= 10:
-#     print(i)
-#     i = i+1
+i = 0
+while i <= 10:
+    print(i)
+    i = i+1
 
 # Countdown
 # Write a while loop that prints numbers from 10 down to 1, then prints "Blast off!".
 
-# i = 10
-# while i >= 1:
-#     print(i)
-#     i = i - 1
-# print("Blast off!")
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+print("Blast off!")
 
 # Sum of Numbers
 # Use a while loop to calculate the sum of numbers from 1 to 100.
 
-# i = 1
-# sum = 0
-# while i <= 100:
-#     sum = sum + i 
-#     i = i + 1
-#     print(sum)
+i = 1
+sum = 0
+while i <= 100:
+    sum = sum + i 
+    i = i + 1
+    print(sum)
 
 # Multiplication Table
 # Ask the user for a number (e.g., 5) and use a while loop to print its multiplication table up to 10.
 
-# num = int(input("enter the number: "))
-# i = 1
-# while i <= 10:
-#     print("5 X", i, "=", num * i)
-#     i = i + 1
+num = int(input("enter the number: "))
+i = 1
+while i <= 10:
+    print("5 X", i, "=", num * i)
+    i = i + 1
 
 # Guess the Number
 # Write a program that keeps asking the user to guess a number until they guess it correctly.
 # (Hint: Use while with input and break when correct.)
 
-# while True:
-#     password = input("enter you password: ")
-#     if password != "happy birthday!":
-#         print("incorrect password")
-#     else:
-#         print("correct password")
-#         break 
+while True:
+    password = input("enter you password: ")
+    if password != "happy birthday!":
+        print("incorrect password")
+    else:
+        print("correct password")
+        break 
 
 # Reverse Digits
 # Use a while loop to reverse the digits of a number.
