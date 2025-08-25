0b3fa6355f9470f31bc68e000f2076bc0939d961
vaidehiadhi <vaidehiadhi2003@gmail.com>
2025-08-25 19:41:34 +0530

    temp

--- day-7/practise.py
+++ day-7/practise.py
@@ -1,16 +1,16 @@
-# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# even = []
-# for i in nums:
-#     if i % 2 == 0:
-#         even.append(i)
-# print(even)
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even = []
+for i in nums:
+    if i % 2 == 0:
+        even.append(i)
+print(even)
 
-# words = ["python", "is", "fun", "and", "powerful"]
-# new_list = []
-# for i in words:
-#     if len(i) >= 3:
-#         new_list.append(i)
-# print(new_list)
+words = ["python", "is", "fun", "and", "powerful"]
+new_list = []
+for i in words:
+    if len(i) >= 3:
+        new_list.append(i)
+print(new_list)
 
 nums = [3, 1, 4, 1, 5, 9, 2, 6]
 square = []
